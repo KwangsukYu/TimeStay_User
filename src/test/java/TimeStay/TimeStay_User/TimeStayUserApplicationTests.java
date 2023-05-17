faae221a25416b9b3cee9abe8894985170645dc1
KwangsukYu <soye0822@gmail.com>
2023-05-17 17:31:26 +0900

    init spring project

---
+++ src/test/java/TimeStay/TimeStay_User/TimeStayUserApplicationTests.java
@@ -0,0 +1,13 @@
+package TimeStay.TimeStay_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeStayUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
