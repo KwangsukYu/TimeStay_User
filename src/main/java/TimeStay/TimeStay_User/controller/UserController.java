f36c002d566cfa10c400864480417dc001522e7c
KwangsukYu <soye0822@gmail.com>
2023-05-24 16:28:15 +0900

    set JSP

--- src/main/java/TimeStay/TimeStay_User/controller/UserController.java
+++ src/main/java/TimeStay/TimeStay_User/controller/UserController.java
@@ -3,6 +3,7 @@ package TimeStay.TimeStay_User.controller;
 import TimeStay.TimeStay_User.dao.UserDAO;
 import TimeStay.TimeStay_User.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/user")
-@RequiredArgsConstructor
 public class UserController {
 
-    private final UserService UserService;
+//    private final UserService UserService;
+//    @GetMapping("/{id}")
+//    public Optional<UserDAO> getUSer(@PathVariable long id) {
+//        return UserService.getUser(id);
+//    }
 
-    @GetMapping("/{id}")
-    public Optional<UserDAO> getUSer(@PathVariable long id) {
-        return UserService.getUser(id);
+    @GetMapping("/test")
+    public String UserHome() {
+        return "index";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Title2</title>
+  </head>
+  <body>
+    <div>
+      <h1>안녕하신가</h1>
+      <h2>진짜 인텔리제이 자동완성좀 시켜주지</h2>
+      <h3>세상에나</h3>
+    </div>
+  </body>
+
+  <script></script>
+</html>
